f87479fe36f4280e1970df1fe691c865b57e0062
vivekpal468 <cse.vivek8279@gmail.com>
2024-08-29 11:20:50 +0530

    netflix clone

---
+++ public/src/Components/Nav.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import "../style/Nav.css";
+
+const Nav = () => {
+  const [show, handleShow] = useState();
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <img
+        className="nav_logo"
+        src="https://images.ctfassets.net/y2ske730sjqp/1aONibCke6niZhgPxuiilC/2c401b05a07288746ddf3bd3943fbc76/BrandAssets_Logos_01-Wordmark.jpg?w=940"
+        alt="Netflix Logo"/>
+    </div>
+  );
+};
+
+export default Nav;
